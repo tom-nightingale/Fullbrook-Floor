7bf9d21aa80f2ab2fa3908f8d6fb675138c37506
Tom Nightingale <tom.nightingale@adtrak.co.uk>
2020-07-15 18:06:00 +0100

    fix spacing throughout

---
+++ wp-content/themes/fullbrook-floor/js/hero-carousel.js
@@ -0,0 +1,27 @@
+var carousel = document.querySelector('.hero-carousel');
+if (carousel) {
+  var slides = document.querySelectorAll('.carousel-image');
+
+  var totalSlides = slides.length;
+  var currentSlide = 0;
+
+  slides[currentSlide].classList.add('active');
+
+  function rotateSlide() {
+    slides.forEach(slide => {
+      slide.classList.remove('active');
+    });
+
+    if (currentSlide < totalSlides) {
+      slides[currentSlide].classList.add('active');
+      currentSlide++;
+    }
+    else {
+      currentSlide = 0;
+      slides[currentSlide].classList.add('active');
+      currentSlide++;
+    }
+  }
+
+  setInterval(rotateSlide, 5000);
+}
