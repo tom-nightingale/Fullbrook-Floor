c0c4c5177333066df8a9019d8fbf646d019389a1
unknown <tom.nightingale@adtrak.co.uk>
2020-07-26 15:38:37 +0100

    add modal form

--- wp-content/themes/fullbrook-floor/dist/production-dist.js
+++ wp-content/themes/fullbrook-floor/dist/production-dist.js
@@ -103,6 +103,25 @@
     });
   }(jQuery));
 
+  const modalTrigger = document.querySelectorAll('.modal-trigger');
+  const modalClose = document.querySelector('.modal-close');
+
+  const modalOverlay = document.querySelector('.modal-overlay');
+  const modalContent = document.querySelector('.modal-content');
+
+  function toggleModal(event, element) {
+      event.preventDefault();
+      modalOverlay.classList.toggle('active');
+      modalContent.classList.toggle('active');
+  }
+
+  modalTrigger.forEach( element => {
+      console.log(element);
+      element.addEventListener('click', toggleModal);
+  });
+
+  modalClose.addEventListener('click', toggleModal);
+
   // binds $ to jquery, requires you to write strict code. Will fail validation if it doesn't match requirements.
   (function($) {
 
---
+++ wp-content/themes/fullbrook-floor/js/modal.js
@@ -0,0 +1,18 @@
+const modalTrigger = document.querySelectorAll('.modal-trigger');
+const modalClose = document.querySelector('.modal-close');
+
+const modalOverlay = document.querySelector('.modal-overlay');
+const modalContent = document.querySelector('.modal-content');
+
+function toggleModal(event, element) {
+    event.preventDefault();
+    modalOverlay.classList.toggle('active');
+    modalContent.classList.toggle('active');
+}
+
+modalTrigger.forEach( element => {
+    console.log(element);
+    element.addEventListener('click', toggleModal);
+});
+
+modalClose.addEventListener('click', toggleModal);
