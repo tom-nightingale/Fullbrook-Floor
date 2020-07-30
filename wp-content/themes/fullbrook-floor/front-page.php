1b7a75310b4cc14f6b621a6c1cb74ae2b44a2445
unknown <tom.nightingale@adtrak.co.uk>
2020-07-30 17:00:20 +0100

    add featured properties to front page

--- wp-content/themes/fullbrook-floor/front-page.php
+++ wp-content/themes/fullbrook-floor/front-page.php
@@ -3,4 +3,17 @@
 $context = Timber::context();
 $context['post'] = new Timber\Post();
 
+$featured_query = [
+    'posts_per_page' => 4,
+    'post_type' => 'property',
+    'meta_query'  => array(
+        array(
+            'key' => '_featured',
+            'value' => 'yes'
+        )
+    )
+];
+$featured_properties = new Timber\PostQuery($featured_query);
+$context['featured_properties'] = $featured_properties;
+
 Timber::render( [ 'front-page.twig' ], $context );
