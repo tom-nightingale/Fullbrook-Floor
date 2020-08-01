a91de4d8ee11c461ef741cd6e0261472292f28f8
unknown <tom.nightingale@adtrak.co.uk>
2020-08-01 16:13:38 +0100

    styling property details

--- wp-content/themes/fullbrook-floor/propertyhive/single-property.php
+++ wp-content/themes/fullbrook-floor/propertyhive/single-property.php
@@ -7,6 +7,20 @@ global $wpdb;
 
 $context = Timber::context();
 
+$terms = [
+    'property_type',
+    'outside_space',
+    'parking',
+    'marketing_flag',
+    'furnished',
+    'tenure',
+    'availability',
+];
+
+foreach($terms as $term) {
+    $context[$term] = get_the_terms($post->ID, $term);
+}
+
 $context['post'] = new Timber\Post();
 
 Timber::render( [ '_propertyhive/single-property.twig', 'page.twig' ], $context );
