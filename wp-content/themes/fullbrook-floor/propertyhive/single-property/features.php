719766026c4ada316356c795747f149a45741807
unknown <tom.nightingale@adtrak.co.uk>
2020-08-01 14:26:42 +0100

    style property features

--- wp-content/themes/fullbrook-floor/propertyhive/single-property/features.php
+++ wp-content/themes/fullbrook-floor/propertyhive/single-property/features.php
@@ -18,12 +18,12 @@ if ( !empty($features) )
 ?>
 
     
-    <ul class="flex-wrap p-4 font-bold lg:flex bg-primary-light bg-opacity-20">
+    <ul class="flex-wrap items-center p-4 font-bold lg:p-8 md:flex bg-primary-light bg-opacity-20 lg:w-4/5">
 <?php
     foreach ($features as $feature)
     {
 ?>
-        <li class="relative py-2 pl-6"><?php icon('check', 'absolute top-3 left-0 text-xs text-secondary h-5 w-5 p-1 rounded-full border border-secondary'); ?> <?php echo $feature; ?></li>
+        <li class="relative py-2 pl-6 md:w-1/2 2xl:w-1/3"><?php icon('check', 'absolute top-3 left-0 text-xs text-secondary h-5 w-5 p-1 rounded-full border border-secondary'); ?> <?php echo $feature; ?></li>
 <?php
     }
 ?>
