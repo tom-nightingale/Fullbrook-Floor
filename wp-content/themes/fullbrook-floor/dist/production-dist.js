1799475ed12b51c0dc2477d11d1c99010ba900ed
unknown <tom.nightingale@adtrak.co.uk>
2020-08-04 22:38:19 +0100

    add active states for main nav for sub pages

--- wp-content/themes/fullbrook-floor/dist/production-dist.js
+++ wp-content/themes/fullbrook-floor/dist/production-dist.js
@@ -178,6 +178,11 @@
 
   modalClose.addEventListener('click', toggleModal);
 
+  var body = document.querySelector('body');
+  if (body.classList.contains('single-help-advice')) {
+      document.querySelector('.menu-item-33').classList.add('current-menu-item');
+  }
+
   // binds $ to jquery, requires you to write strict code. Will fail validation if it doesn't match requirements.
   (function($) {
 
---
+++ wp-content/themes/fullbrook-floor/js/nav-active-states.js
@@ -0,0 +1,4 @@
+var body = document.querySelector('body');
+if (body.classList.contains('single-help-advice')) {
+    document.querySelector('.menu-item-33').classList.add('current-menu-item');
+}
