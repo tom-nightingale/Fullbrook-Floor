5bbdc9f16455961bd29a564d6d5a955048042728
unknown <tom.nightingale@adtrak.co.uk>
2020-08-12 22:16:08 +0100

    edit max price in search form

--- wp-content/themes/fullbrook-floor/_functions/propertyhive-search.php
+++ wp-content/themes/fullbrook-floor/_functions/propertyhive-search.php
@@ -8,5 +8,27 @@ function edit_default_property_search_form_fields($fields)
     // Remove the minimum bedrooms dropdown
     unset($fields['department']);
 
+
+    //add additional fields to the property maximum price
+     $prices = array(
+        '' => __( 'No preference', 'propertyhive' ),
+        '100000' => '£100,000',
+        '150000' => '£150,000',
+        '200000' => '£200,000',
+        '250000' => '£250,000',
+        '300000' => '£300,000',
+        '500000' => '£500,000',
+        '750000' => '£750,000',
+        '1000000' => '£1,000,000',
+        '1250000' => '£1,250,000',
+        '1500000' => '£1,500,000',
+        '2000000' => '£2,000,000',
+        '3000000' => '£3,000,000',
+        '4000000' => '£4,000,000',
+        '5000000' => '£5,000,000',
+    );
+    $fields['maximum_price']['options'] = $prices;
+
+
     return $fields; // return the fields
 }
