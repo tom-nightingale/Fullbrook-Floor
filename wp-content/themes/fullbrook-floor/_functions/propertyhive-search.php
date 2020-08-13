a6473b2c5114b230849b32dc6cde41ae1a0ab6b2
unknown <tom.nightingale@adtrak.co.uk>
2020-08-13 18:10:35 +0100

    update enquiry form function typo

--- wp-content/themes/fullbrook-floor/_functions/propertyhive-search.php
+++ wp-content/themes/fullbrook-floor/_functions/propertyhive-search.php
@@ -37,7 +37,7 @@ function edit_default_property_search_form_fields($fields)
 // Update the enquiry form to have a nice Telephone Number label
 add_filter( 'propertyhive_property_enquiry_form_fields', 'edit_default_property_enquiry_form' );
 
-function edit_default_property_enquiry_form($fields)
+function edit_default_property_enquiry_form($fields) {
 
     $fields['telephone_number']['label'] = "Telephone Number";
 
