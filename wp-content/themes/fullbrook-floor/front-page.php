d8f5609c37ab320faddcc9366cac9f0ecb8c1676
unknown <tom.nightingale@adtrak.co.uk>
2020-10-05 16:49:35 +0100

    update featured properties

--- wp-content/themes/fullbrook-floor/front-page.php
+++ wp-content/themes/fullbrook-floor/front-page.php
@@ -4,15 +4,15 @@ $context = Timber::context();
 $context['post'] = new Timber\Post();
 
 $featured_query = [
-    'posts_per_page' => 4,
-    'orderby' => 'rand',
+    'posts_per_page' => 3,
+    'orderby' => 'date',
     'post_type' => 'property',
-    'meta_query'  => array(
-        array(
-            'key' => '_featured',
-            'value' => 'yes'
-        )
-    )
+    // 'meta_query'  => array(
+    //     array(
+    //         'key' => '_featured',
+    //         'value' => 'yes'
+    //     )
+    // )
 ];
 $featured_properties = new Timber\PostQuery($featured_query);
 $context['featured_properties'] = $featured_properties;
