0f075d37744a4a2f632a8c9b92157b51146d7b30
unknown <tom.nightingale@adtrak.co.uk>
2021-01-14 17:24:55 +0000

    remove back to top

--- wp-content/themes/fullbrook-floor/js/back-to-top.js
+++ wp-content/themes/fullbrook-floor/js/back-to-top.js
@@ -6,28 +6,28 @@
 // --------------------------------------------------------------------------------------------------
 
 // // Set a variable to identify the back to top button
-var backToTopBtn = document.getElementById("back-top")
+// var backToTopBtn = document.getElementById("back-top")
 
-var observer = new IntersectionObserver(entries => {
-  if (entries[0].boundingClientRect.y < 0) {
-    // Add the opacity class to bring in the 
-    backToTopBtn.classList.add("opacity-25")
-    backToTopBtn.classList.remove("opacity-0")
-    // Bring it into the viewport (removing from the viewport means it isn't clickable)
-    backToTopBtn.classList.remove("-mb-20")
-  } else {
-    backToTopBtn.classList.remove("opacity-25")
-    backToTopBtn.classList.add("opacity-0")
-    // Move it back out of the viewport
-    backToTopBtn.classList.add("-mb-20")
-  }
-});
-observer.observe(document.querySelector("#pixel-to-watch"));
+// var observer = new IntersectionObserver(entries => {
+//   if (entries[0].boundingClientRect.y < 0) {
+//     // Add the opacity class to bring in the 
+//     backToTopBtn.classList.add("opacity-25")
+//     backToTopBtn.classList.remove("opacity-0")
+//     // Bring it into the viewport (removing from the viewport means it isn't clickable)
+//     backToTopBtn.classList.remove("-mb-20")
+//   } else {
+//     backToTopBtn.classList.remove("opacity-25")
+//     backToTopBtn.classList.add("opacity-0")
+//     // Move it back out of the viewport
+//     backToTopBtn.classList.add("-mb-20")
+//   }
+// });
+// observer.observe(document.querySelector("#pixel-to-watch"));
 
-backToTopBtn.onclick = function () {
-  window.scroll({
-    top: 0,
-    left: 0,
-    behaviour: "smooth",
-  })
-}
+// backToTopBtn.onclick = function () {
+//   window.scroll({
+//     top: 0,
+//     left: 0,
+//     behaviour: "smooth",
+//   })
+// }
